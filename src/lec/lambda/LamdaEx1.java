62ac55b75b2bd8bbcbc600b284f90d633f6e7cfc
sunabove <sunabove@nate.com>
2021-02-03 08:12:19 +0900

    **

---
+++ src/lec/lambda/LamdaEx1.java
@@ -0,0 +1,38 @@
+package lec.lambda;
+
+import java.util.ArrayList;
+
+public class LamdaEx1 {
+    public static void main(String[] args) {
+    	var line = "*".repeat( 80 );
+    	var out = System.out;
+    	
+        var numbers = new ArrayList<Integer>();
+        
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(1);
+        
+        out.println( line );
+        out.println( "lambda expression" );
+
+        numbers.forEach((n) -> {
+            out.println(n);
+        });
+        
+        out.println( line );
+        out.println( "for : " );
+        
+        for( var n : numbers ) {
+        	out.println(n);
+        }
+        
+        out.println( line );
+        out.println( "for i" );
+        
+        for(int i = 0 ; i < numbers.size() ; i++ ) {
+        	out.println(numbers.get(i));
+        }
+    }
+}
