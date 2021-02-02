864c22491777a8ece5fdecd4ea608656b4eb3ab9
sunabove <sunabove@nate.com>
2021-02-02 17:41:41 +0900

    홍지수 패턴 G 작성

---
+++ src/lec/PatternG.java
@@ -0,0 +1,23 @@
+package lec;
+
+public class PatternG {
+	// 홍지수님 작성
+
+	public static void main(String[] args) { 
+
+		for (int i = 1; i < 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+
+			}
+			System.out.println();
+		}
+
+		for (int i = 5; i > 0; i--) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
