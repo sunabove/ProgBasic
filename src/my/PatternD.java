63d092dc26662e9328dda99affcde1fa52e2cd23
DONGSU <dlehdtn0401@gmail.com>
2021-02-02 09:50:01 +0900

    **

--- src/my/PatternD.java
+++ src/my/PatternD.java
@@ -5,6 +5,7 @@ public class PatternD {
 	public static void main(String [] args) { 
 		System.out.println( "Hello 성병문" );
 		System.out.println( "Hello 박보람" );
+		System.out.println( "Hello 이동수" );
 	}
 
 }
