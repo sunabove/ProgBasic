b1a3db3c8a207d36b1ec7c620a0883fe3f26d8d6
sunabove <sunabove@nate.com>
2021-02-03 10:07:21 +0900

    **

---
+++ src/lec/Ex7Fibonacci.java
@@ -0,0 +1,19 @@
+package lec;
+
+public class Ex7Fibonacci {
+
+	public static void main(String[] args) {
+		var out = System.out;
+		
+		int f0 = 1;
+		int f1 = 1;
+		
+		int idx = 1; 
+		while( f0 <= f1) {
+			out.println( "".format( "f[%d] = %d", idx, f1 ) );
+			
+			idx ++ ; 
+		}
+	}
+
+}
