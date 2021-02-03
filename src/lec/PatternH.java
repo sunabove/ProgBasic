95bda757aa50f6993585686506658dd2ac26721c
sunabove <sunabove@nate.com>
2021-02-03 14:36:31 +0900

    **

--- src/lec/PatternH.java
+++ src/lec/PatternH.java
@@ -1,16 +1,17 @@
 package lec;
 
-public class PatternG2 {	
-	// 박보람님 작성, 성병문 수정 
+public class PatternH {	 
 
 	public static void main(String[] args) {
 		var out = System.out; 
 		
 		var lineNo = 10 ; 
 		for (int i = 0; i < lineNo ; i++ ) {
-			int cnt = i < 5 ? i + 1 : ( lineNo - i - 1 ) ; 
+			int cnt = i < 5 ? i + 1 : ( lineNo - i ) ; 
 			
-			var text = "* ".repeat( cnt );
+			var text = "*".repeat( cnt );
+			
+			//var space = 
 			
 			out.println( text );
 		}
