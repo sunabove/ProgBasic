69f20388e8715231fda719a9058b746c5fc52cff
sunabove <sunabove@nate.com>
2021-02-03 11:25:55 +0900

    쓰레드 예제

---
+++ src/lec/thread/Main.java
@@ -0,0 +1,19 @@
+package lec.thread;
+
+public class Main implements Runnable {
+	
+	public static void main( String [] args ) {
+		Main runnable = new Main(); // 실행할 코드 
+		
+		Thread thread = new Thread( runnable);
+		thread.start();
+		
+		System.out.println( "This code is outside of the thread." );
+	}
+
+	@Override // Annotation 
+	public void run() {
+		System.out.println( "This code is running inside the thread." ); 
+	}
+	
+}
