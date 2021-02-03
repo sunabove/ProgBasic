216142d0763be01c196509438f50e5d9f839e857
sunabove <sunabove@nate.com>
2021-02-03 15:07:38 +0900

    공경택 님 패턴 H 코드

---
+++ src/lec/PatternHGgt.java
@@ -0,0 +1,45 @@
+package lec;
+
+public class PatternHGgt {
+	// 공경택 님 작성 
+
+	public static void main(String[] args) {
+	
+		int a =1;
+		int b =3;
+		
+		for(int i=0; i<7 ;i++) {
+			if(i < 3) {
+				for(int j = b; j>0 ; j--) {
+					System.out.print(" ");
+				}
+				for(int j= 0 ;j < a ; j++) {
+					System.out.print("*");
+				}
+				for(int j = b; j >0 ; j--) {
+					System.out.print(" ");
+				}
+				System.out.println();
+				
+				a+=2;
+				b--;
+			}else {
+				for(int j=0; j<b ; j++) {
+					System.out.print(" ");
+				}
+				for(int j=a; j>0 ; j--) {
+					System.out.print("*");
+				}
+				for(int j=0;j<b;j++) {
+					System.out.print(" ");
+				}
+				System.out.println();
+				a-=2;
+				b++;
+			}
+			
+			
+		}
+	}
+
+}
