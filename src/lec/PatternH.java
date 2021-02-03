e53eec57bb39afb61384976d4fa0c69c6594efc3
sunabove <sunabove@nate.com>
2021-02-03 15:02:54 +0900

    문제 H: 패턴 출력

--- src/lec/PatternH.java
+++ src/lec/PatternH.java
@@ -2,18 +2,20 @@ package lec;
 
 public class PatternH {	 
 
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 		var out = System.out; 
 		
 		var lineNo = 10 ; 
 		for (int i = 0; i < lineNo ; i++ ) {
-			int cnt = i < 5 ? i + 1 : ( lineNo - i ) ; 
+			int cnt = i < 5 ? i + 1 : ( lineNo - i - 1 ) ; 
 			
-			var text = "*".repeat( cnt );
+			var text = "* ".repeat( cnt );
 			
-			//var space = 
+			var leadCnt = 5 - cnt ; 
+			var leading = leadCnt < 1 ? "" : " ".repeat( leadCnt ) ; 
 			
-			out.println( text );
+			out.println( "".format( "%s%s", leading, text ) );
 		}
 	}
 
