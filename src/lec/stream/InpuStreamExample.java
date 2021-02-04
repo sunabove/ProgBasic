662c543aca1813439a5eab600121d766256a0fa2
sunabove <sunabove@nate.com>
2021-02-04 14:43:30 +0900

    **

--- src/lec/stream/InpuStreamExample.java
+++ src/lec/stream/InpuStreamExample.java
@@ -5,9 +5,12 @@ import java.io.FileInputStream;
 public class InpuStreamExample {
 	public static void main(String args[]) throws Exception {
 		FileInputStream fin = new FileInputStream("C:\\Temp\\testout.txt");
+		
 		int i = fin.read();
+		char c = (char) i; 
+		
 		System.out.println( i );
-		System.out.println((char) i);
+		System.out.println( c );
 
 		fin.close();
 	}
--- src/lec/stream/OutputStreamExample.java
+++ src/lec/stream/OutputStreamExample.java
@@ -5,8 +5,10 @@ import java.io.FileOutputStream;
 public class OutputStreamExample {
 	public static void main(String args[]) throws Exception {
 		FileOutputStream fout = new FileOutputStream("C:\\Temp\\testout.txt");
+		
 		fout.write(65);
 		fout.close();
+		
 		System.out.println("success...");
 	}
 }
