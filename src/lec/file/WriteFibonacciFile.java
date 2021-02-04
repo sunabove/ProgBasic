7734189bbd2f33b524ebfd72e69ed075d1cd1d80
sunabove <sunabove@nate.com>
2021-02-04 11:44:03 +0900

    **

--- src/lec/file/WriteFibonacciFile.java
+++ src/lec/file/WriteFibonacciFile.java
@@ -4,16 +4,36 @@ import java.io.FileWriter; // Import the FileWriter class
 import java.io.IOException; // Import the IOException class to handle errors
 
 public class WriteFibonacciFile {
+	static int fibonacci(int n) {
+		// 적당히 수정
+		if (n <= 2) {
+			return 1;
+		}
+
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
 	public static void main(String[] args) throws Exception {
 		var out = System.out;
 		FileWriter fileWriter = new FileWriter("c:\\temp\\fibonacci.csv");
+
 		
-		for( int i = 0 ; i < 10; i ++ ) {
-			fileWriter.write("Hello....\n");
+		out.println( "Hello.." );
+		
+		int f;
+		String text ; 
+		for(int i = 0; i < 10; i++) {
+			f = fibonacci( i );
+			text = String.format( "%d, %d\n", i, f );
+			out.print( text );
+			fileWriter.write( text );
 		}
 		
+		fileWriter.flush();
+
 		fileWriter.close();
-		
+
 		out.println("Successfully wrote to the fibonacci.csv file.");
 	}
+
 }
