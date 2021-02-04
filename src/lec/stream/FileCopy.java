18de9750ae7536f84014a644bbaf0f8fb43dd503
sunabove <sunabove@nate.com>
2021-02-04 16:09:32 +0900

    **

--- src/lec/stream/FileCopy.java
+++ src/lec/stream/FileCopy.java
@@ -12,11 +12,18 @@ public class FileCopy {
 		var in = new FileInputStream("C:\\Temp\\ascii.txt");
 		var out = new FileOutputStream("C:\\Temp\\my_ascii.txt");
 		
-		int i = in.read();
+		int i ;
+		
+		// 파일에서 한 바이트 읽어들임.
+		i = in.read();
 		
 		while( i > -1 ) {
-			// out ......
 			sout.println( "char = " + (char) i );
+			
+			// 파일에서 한 바이트 씀.
+			out.write( i );
+			// 파일에서 한 바이트 읽어들임.
+			i = in.read(); 
 		}
 
 		out.flush();
