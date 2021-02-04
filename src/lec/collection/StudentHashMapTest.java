d3882ee3b465759302c98f9a649836e08f9ddbe8
sunabove <sunabove@nate.com>
2021-02-04 11:02:07 +0900

    실습 : HashMap 예제 틀

---
+++ src/lec/collection/StudentHashMapTest.java
@@ -0,0 +1,26 @@
+package lec.collection;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class StudentHashMapTest {
+
+	public static void main(String[] args) {
+		var noNameMap = new HashMap<Integer, String>();
+		
+		var no = 0 ; 
+		noNameMap.put( no ++ ,  "Jone" );
+		noNameMap.put( no ++ ,  "Brown" );
+		
+		System.out.println( "*".repeat(40));
+		
+		// 가장 높은 번호의 키를 구한다.
+		for ( var key : noNameMap.keySet()) { 
+			System.out.println("No = " + key); 
+		}
+		
+		// 가장 높은 키값의 이름을 구하여 출력한다.
+	}
+
+}
