7bce8fdb87efcf690722ec19c3e0e19bd1e864b3
sunabove <sunabove@nate.com>
2021-02-04 15:38:25 +0900

    **

--- src/lec/Abc.java
+++ src/lec/Abc.java
@@ -1,10 +1,6 @@
 package lec;
 
-public class Abc {
-	
-	// Abc í�´ëž˜ìŠ¤ì�˜ ì�´ë¦„ì�€ ?
-	// my.Abc : í�´ëž˜ìŠ¤ ì�´ë¦„
-	// Abc ? Simple Class Name , ê°„ë‹¨ í�´ëž˜ìŠ¤ ëª…
+public class Abc { 
 
 	public static void main(String[] args) {
 		System.out.println( "my.Abc Hello..." ); 
