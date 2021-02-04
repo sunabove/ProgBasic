bef2b4b3d5871a05a09717809d0918f9baa9e979
sunabove <sunabove@nate.com>
2021-02-04 09:35:50 +0900

    ArrayListIntegerTest

---
+++ src/lec/collection/ArrayListIntegerTest.java
@@ -0,0 +1,19 @@
+package lec.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayListIntegerTest {
+	public static void main(String[] args) {
+		var numbers = new ArrayList<Integer>();
+		
+		numbers.add( 1 );
+		numbers.add( 2 );
+		numbers.add( 5 );
+		numbers.add( 9 );
+		
+		for (var number : numbers) {
+			System.out.println( number );
+		}
+	}
+}
