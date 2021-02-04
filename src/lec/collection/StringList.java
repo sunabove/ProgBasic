5bb598f38761c7e9d6d9e5d842718eb655fb5c30
sunabove <sunabove@nate.com>
2021-02-04 09:46:16 +0900

    **

--- src/lec/collection/StringList.java
+++ src/lec/collection/StringList.java
@@ -6,6 +6,7 @@ public class StringList extends java.util.ArrayList<String>{
 
 	private static final long serialVersionUID = -5238731462108700329L;
 	
+	// 생성자 3개를 만듦.
 	public StringList() {
 		super();
 	}
@@ -18,6 +19,8 @@ public class StringList extends java.util.ArrayList<String>{
 	public StringList(Collection<? extends String> c) {
 		super( c );
 	}
+	
+	// 리스트의 크기를 반환하는 length() 함수를 만듦.
 	
 	public int length() {
 		return super.size();
