3b630cde139ae1f283b7aa8f35785a6212907853
sunabove <sunabove@nate.com>
2021-02-05 09:27:07 +0900

    가변인자와 고장인자

--- src/lec/VarArgsExample.java
+++ src/lec/VarArgsExample.java
@@ -1,19 +1,26 @@
 package lec;
 
 public class VarArgsExample {
+	
+	// static void display(int i, String ... values , int k) { }
+	// 문법적으로 허용이 안 됨. 
+	// 고정인자는 가변인자 앞에 와야 함.
 
-	static void display(String ... values ) {
+	static void display(int i, String ... values) {
 		//System.out.println("display method invoked.");
 		
-		for (String s : values) {
+		String [] texts = values ;
+		
+		for (String s : texts ) {
 			System.out.print(s + ", ");			
 		}
+		
 		System.out.println();
 	}
 
 	public static void main(String args[]) {
-		display();// zero argument
-		display("hello");// one argument
-		display("my", "name", "is", "varargs");// four arguments
+		display( 1 );// zero argument
+		display( 2, "hello");// one argument
+		display( 3, "my", "name", "is", "varargs");// four arguments
 	}
 }
