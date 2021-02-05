60a6ce7946b1d90263265841b1cca81685f59682
sunabove <sunabove@nate.com>
2021-02-05 16:09:41 +0900

    박 보람님 HashMap 작성

---
+++ src/lec/collection/StudentHashMapTestPbr2.java
@@ -0,0 +1,34 @@
+package lec.collection;
+
+import java.util.HashMap;
+
+public class StudentHashMapTestPbr2 {
+	// 박보람 님 작성 
+
+	public static void main(String[] args) {
+		
+		
+		HashMap <Integer, String> s = new HashMap<Integer, String>();
+		int idx = 1;
+		int idxpre = 1;
+		int max = idx;
+		
+		s.put(idx++, "박보람");
+		s.put(idx++, "공경택");
+		s.put(idx++, "이동수");
+		s.put(idx++, "정한민");
+		s.put(idx++, "홍지수");
+		
+		for (Integer k : s.keySet()) {
+			if (k > max) {
+				idxpre = max;
+				max = k;
+			}
+		}
+		
+		System.out.println(s.get(max));
+		System.out.println("pre " + s.get(idxpre));  
+
+	}
+
+}
