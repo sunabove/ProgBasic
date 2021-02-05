72a847583e451e53d81d6316b28571ddb8688687
sunabove <sunabove@nate.com>
2021-02-06 06:37:26 +0900

    **

--- src/lec/net_02_readwrite/MyClientReadWrite.java
+++ src/lec/net_02_readwrite/MyClientReadWrite.java
@@ -8,21 +8,22 @@ class MyClientReadWrite {
 		var sout = System.out;
 		
 		sout.println( "Connecting to the server...." );
-		Socket socket = new Socket("localhost", 3333);
+		var socket = new Socket("localhost", 3333);
 		sout.println( "Connected to the server." );
 		
-		DataInputStream in = new DataInputStream(socket.getInputStream());
-		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		var in = new DataInputStream(socket.getInputStream());
+		var out = new DataOutputStream(socket.getOutputStream());
+		var console = new BufferedReader(new InputStreamReader(System.in));
 
-		String str = "", str2 = "";
+		var str = "";
+		var str2 = "";
 		while (!str.equals("stop")) {
 			str = console.readLine();
 			out.writeUTF(str);
 			out.flush();
 			
 			str2 = in.readUTF();
-			System.out.println("Server says: " + str2);
+			sout.println("Server says: " + str2);
 		}
 
 		out.close();
--- src/lec/net_02_readwrite/MyServerReadWrite.java
+++ src/lec/net_02_readwrite/MyServerReadWrite.java
@@ -7,22 +7,22 @@ class MyServerReadWrite {
 	public static void main(String args[]) throws Exception {
 		var sout = System.out; 
 		
-		ServerSocket serverSocket = new ServerSocket(3333);
+		var serverSocket = new ServerSocket(3333);
 		
 		sout.println( "Waiting a client ..." );	
-		Socket socket = serverSocket.accept();
+		var socket = serverSocket.accept();
 		sout.println( "A client has been accepted." ); 
 		
-		DataInputStream in = new DataInputStream(socket.getInputStream());
-		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-		BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+		var in = new DataInputStream(socket.getInputStream());
+		var out = new DataOutputStream(socket.getOutputStream());
+		var console = new BufferedReader(new InputStreamReader(System.in));
 
 		var strClient = "";
 		var strConsole = "";
 		
 		while (! strClient.equals("stop")) {
 			strClient = in.readUTF();
-			System.out.println("client says: " + strClient);
+			sout.println("client says: " + strClient);
 			strConsole = console.readLine();
 			out.writeUTF(strConsole);
 			out.flush();
