b24c36e6c53d858f6f61a359ca7508e6790671be
sunabove <sunabove@nate.com>
2021-02-06 12:43:42 +0900

    **

--- src/lec/net_05_room/MyServerRoom.java
+++ src/lec/net_05_room/MyServerRoom.java
@@ -6,43 +6,78 @@ import java.io.*;
 
 public class MyServerRoom {
 	
-	static ArrayList<SendThread> sendThreadList = new ArrayList<>();
+	private int threadId = 0 ;
 	
-	static ArrayList<String> msgList = new ArrayList<>();
-	static int msgCount = 0 ; 
+	private ArrayList<SendThread> sendThreadList = new ArrayList<>();	
+	private ArrayList<String> msgList = new ArrayList<>();
+	private int msgCount = 0 ; 
 	
-	static synchronized void sendToAllClient( SendThread currThread ) {
+	synchronized void sendToAllClient( SendThread currThread ) {
 		while( msgList.size() > 0 ) {
 			var msg = msgList.get(0);
 			msgList.remove( 0 );
 			
 			// 모든 클라이언트에 메시지 전송 
-			for( var sendThread : sendThreadList ) {
-				if( sendThread != currThread ) { 
+			var sendThreadList = this.sendThreadList;
+			
+			for( int i = 0 ; i < sendThreadList.size() ; i ++  ) {
+				var sendThread = sendThreadList.get(i); 
+				
+				if( sendThread != null && sendThread != currThread ) { 
 					try {
 						sendThread.out.writeUTF( msg );
 					} catch (IOException e) {
 						// 에러가 발생하면 접속이 종료된 간주함.
-						sendThread.stop = true;
-						//  쓰레드 목록에서 제외한다.
-						sendThreadList.remove( sendThread );
+						sendThread.stopThread(); 
+						i -- ; 
 					}
 				}
 			}
 		}
 	}
 	
-	static class SendThread extends Thread {
-		Socket socket ; 
-		DataInputStream in ; 
-		DataOutputStream out ; 
+	public void startServer() throws Exception {
+		var sout = System.out; 
 		
-		boolean stop = false ; 
+		var serverSocket = new ServerSocket(3333);
+		
+		sout.println( "Waiting a client ..." );	
+		Socket socket ;
+		while( ( socket = serverSocket.accept() ) != null ) {
+			sout.println( "A client has been accepted." ); 
+			
+			var sendThread = new SendThread( socket ); 
+			sendThread.start();
+		} 
+		
+		serverSocket.close();
+		
+		sout.println( "Good bye!" );
+	}
+	
+	class SendThread extends Thread {
+		private DataInputStream in ; 
+		private DataOutputStream out ; 
+		
+		private boolean stop = false ;
 		
 		public SendThread( Socket socket ) throws Exception {
-			this.socket = socket ; 
+			super( "" + threadId );
+			threadId ++;
+			
 			this.in = new DataInputStream( socket.getInputStream());
 			this.out = new DataOutputStream( socket.getOutputStream());
+		}
+		
+		public void stopThread() {
+			this.stop = true ;
+			
+			//  쓰레드 목록에서 제외한다.
+			if( sendThreadList.contains(this)) { 
+				sendThreadList.remove( this );
+			}
+			
+			this.interrupt();
 		}
 		
 		public void run() {
@@ -85,24 +120,8 @@ public class MyServerRoom {
 	}
 	
 	public static void main(String args[]) throws Exception {
+		var server = new MyServerRoom();
+		server.startServer();
 	}
 	
-	public void startServer() throws Exception {
-		var sout = System.out; 
-		
-		var serverSocket = new ServerSocket(3333);
-		
-		sout.println( "Waiting a client ..." );	
-		Socket socket ;
-		while( ( socket = serverSocket.accept() ) != null ) {
-			sout.println( "A client has been accepted." ); 
-			
-			var sendThread = new SendThread( socket ); 
-			sendThread.start();
-		} 
-		
-		serverSocket.close();
-		
-		sout.println( "Good bye!" );
-	}
 }
