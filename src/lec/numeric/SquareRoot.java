a2c8d4224b486bc0779c5fab9444a2dae1f9bf60
sunabove <sunabove@nate.com>
2021-02-08 23:49:42 +0900

    Square Root Example

---
+++ src/lec/numeric/SquareRoot.java
@@ -0,0 +1,53 @@
+package lec.numeric;
+
+import java.io.PrintStream;
+
+public class SquareRoot {
+	
+	PrintStream out = System.out;
+	double num = 0.0;
+	
+	public SquareRoot(double num) {
+		this.num = num;
+	}
+	
+	public double f(double x) {
+		return x*x - num ;
+	}
+	
+	public double f1(double x) {
+		return 2*x;
+	}
+	
+	public double solve() {
+		var x = 100.0;
+		var epsilon = 0.000001;
+		
+		var idx = 0 ; 
+		
+		var f = 0.0;
+		var f1 = 0.0 ; 
+		do {
+			f = f(x) ; 
+			f1 = f1(x) ;
+			
+			out.println( String.format( "[%04d] f(%.6f) = %.6f", idx, x, f ) );
+			
+			x = x - f/f1 ;			
+			idx ++ ; 
+		} while( Math.abs( f ) > epsilon );
+		
+		return x;
+	}
+
+	public static void main(String[] args) {
+		var num = 2.0;
+		var newtonRaphson = new SquareRoot( num );
+		
+		var x = newtonRaphson.solve();
+		
+		System.out.println( String.format("\nSquare root of %f = %f", num, x ) );
+
+	}
+
+}
