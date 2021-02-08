f7ccf1bacf97b609d8952b6f38851e82d9e425c6
sunabove <sunabove@nate.com>
2021-02-08 16:17:24 +0900

    Newton Raphson 수치 해석 예

---
+++ src/lec/numeric/NewtonRaphson.java
@@ -0,0 +1,50 @@
+package lec.numeric;
+
+import java.io.PrintStream;
+
+public class NewtonRaphson {
+	
+	PrintStream out = System.out;
+	
+	public NewtonRaphson() {
+	}
+	
+	public double f(double x) {
+		return x*x - x - 2;
+	}
+	
+	public double f1(double x) {
+		return 2*x - 1;
+	}
+	
+	public double solve() {
+		var x = 100.0;
+		var epsilon = 0.000001;
+		
+		var idx = 0 ; 
+		
+		var f = 0.0;
+		var f1 = 0.0 ; 
+		do {
+			f = f(x) ; 
+			f1 = f1(x) ;
+			
+			out.println( String.format( "[%04d] %.6f = f(%.6f)", idx, f, x ) );
+			
+			x = x - f/f1 ;			
+			idx ++ ; 
+		} while( Math.abs( f ) > epsilon );
+		
+		return x;
+	}
+
+	public static void main(String[] args) {
+		var newtonRaphson = new NewtonRaphson();
+		
+		var x = newtonRaphson.solve();
+		
+		System.out.println( String.format("solution = %f", x ) );
+
+	}
+
+}
